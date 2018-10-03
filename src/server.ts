b5667e0ae24a50fe30e777ad14fc229d9b8f886d
Alassane Coly-Arogundade <bonjour@alassanecoly.fr>
2018-10-03 10:28:00 +0200

    chore: Configure base project with React + Next.js + TypeScript

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import {createServer, IncomingMessage, ServerResponse} from 'http';
+import {parse} from 'url';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev, dir: './src'});
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+	createServer((req: IncomingMessage, res: ServerResponse) => {
+		const parsedUrl = parse(req.url as string, true);
+		const {pathname, query} = parsedUrl;
+
+		if (pathname === '/a') {
+			app.render(req, res, '/b', query);
+		} else if (pathname === '/b') {
+			app.render(req, res, '/a', query);
+		} else {
+			handle(req, res, parsedUrl);
+		}
+	}).listen(3000, err => {
+		if (err) {
+			throw err;
+		}
+		console.log('> Ready on http://localhost:3000');
+	});
+});
