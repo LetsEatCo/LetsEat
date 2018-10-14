5e1064746f27f29a10e5ba6ceafda5ffb0d23fc1
Alassane Coly-Arogundade <bonjour@alassanecoly.fr>
2018-10-14 20:03:59 +0200

    chore(deps): Add Rebass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import {Box, Flex, Heading, Text, Button} from 'rebass';
 
-const Title = styled.h1`
-	color: black;
-	font-size: 50px;
-`;
-
-export default () => {
-	return <Title>Welcome to Next.js !</Title>;
-};
+export default class Index extends React.Component {
+	render() {
+		return (
+			<Text fontFamily="system-ui,sans-serif">
+				<Box px={3} py={5} color="white" bg="blue">
+					<Heading as="h1" fontSize={[4, 5, 6]}>
+						Hello, Next.js
+					</Heading>
+				</Box>
+				<Flex px={3} py={4} alignItems="center">
+					<Heading color="blue">Hello</Heading>
+					<Box mx="auto" />
+					<Button>Hey</Button>
+				</Flex>
+			</Text>
+		);
+	}
+}
